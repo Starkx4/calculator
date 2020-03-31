adfefcd2c4bd0af9992e34450a46f77ac54b143e
Starkx4 <58329399+Starkx4@users.noreply.github.com>
2020-03-31 21:17:39 +0530

    cal.c

---
+++ cal.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int d, x, n;
+printf("\n ENTER DIAMETER and N");
+scanf(%d %n; &d,&n);
+x= d*(sin(360/(2*n));
+printf("\n x= ",&x);
+getch()
+}
